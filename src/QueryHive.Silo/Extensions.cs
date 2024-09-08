57fed2e9b829ea81ef581274762cb0eef86f3f5a
Kia Raad <kia.panahirad@gmail.com>
2024-09-08 10:11:17 +0330

    Add extension method to setup Silo
    This extension method will be used in any host project for example
    a web application or a console app

---
+++ src/QueryHive.Silo/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace QueryHive.Silo;
+public static class Extensions
+{
+    public static IHostApplicationBuilder AddQueryHive(this IHostApplicationBuilder builder)
+    {
+        builder.AddServiceDefaults();
+
+        builder.AddKeyedRedisClient("hive-redis");
+
+        builder.UseOrleans();
+
+        return builder;
+    }
+}
