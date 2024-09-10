8f27fdc45c5f75947896dfe6bcaadcbec80b96ac
Kia Raad <kia.panahirad@gmail.com>
2024-09-10 13:25:56 +0330

    Refactor SearchTermModel and add validation methods
    Renamed properties in SearchTermModel:
    - GoogleSearchPageLink to SearchPage
    - GoogleSearchResultLink to ResultPage
    
    Added methods:
    - IsSearchLinkPopulated: checks if SearchPage is populated
    - IsResultLinkPopulated: checks if ResultPage is populated
    - SetSearchLink: sets SearchPage with validation
    - SetResultLink: sets ResultPage with validation

--- src/QueryHive.Silo/Models/SearchTermModel.cs
+++ src/QueryHive.Silo/Models/SearchTermModel.cs
@@ -10,14 +10,41 @@ public sealed class SearchTermModel
     [Id(0)]
     public required string Term { get; set; }
     [Id(1)]
-    public required string GoogleSearchPageLink { get; set; }
+    public required string SearchPage { get; set; }
     [Id(2)]
-    public required string GoogleSearchResultLink { get; set; }
+    public required string ResultPage { get; set; }
     [Id(3)]
     public DateTimeOffset CreationDate { get; set; }
 
     public override string ToString()
     {
         return JsonSerializer.Serialize(this);
+    }
+
+    public bool IsSearchLinkPopulated() => !string.IsNullOrEmpty(SearchPage);
+    public bool IsResultLinkPopulated() => !string.IsNullOrEmpty(ResultPage);
+
+    ///<summary>
+    /// Sets the search link for the search term.
+    /// </summary>
+    /// <param name="link">The search link to set.</param>
+    /// <exception cref="ArgumentException" >Thrown when the link is null or empty.</exception>
+    public void SetSearchLink(string link)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(link, nameof(link));
+
+        SearchPage = link;
+    }
+
+    ///<summary>
+    /// Sets the result link for the search term.
+    /// </summary>
+    /// <param name="link">The search link to set.</param>
+    /// <exception cref="ArgumentException" >Thrown when the link is null or empty.</exception>
+    public void SetResultLink(string link)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(link, nameof(link));
+
+        ResultPage = link;
     }
 }
